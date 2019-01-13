8cedaef7e02e338e25d86f93ef8f06415e9f2a15
Jonathan <Jonathan@iMac-3.local>
2019-01-12 21:08:05 -0500

    3

--- public/script.js
+++ public/script.js
@@ -1,33 +1,29 @@
 $( document ).ready( function(){
 
-const library = {},
-    source = '#source-text', 
-    userInput = '#user-input'
+const library = {}
+const threeLibrary = {}
+const source = '#source-text'
+const userInput = '#user-input'
 
-const getWords = function(s){
+const getWords = function(s, removeSentences = true){
     const space = new RegExp(/\n/g)
-    const regexp = new RegExp(/\,|\.|\:|\(|\)|\;|\'|\"|\↵/g)
+    const regexp = removeSentences ? new RegExp(/\,|\.|\:|\(|\)|\;|\'|\"|\↵/g) : new RegExp(/\,|\:|\(|\)|\;|\'|\"|\!|\?/g)
     return $(s).val().toLowerCase().replace(space,' ').replace(regexp,'').split(' ').filter(e=>e!='')
 }
 
 
-const store = () => {
+const store = function(){
     const words = getWords(source)
     words.forEach((e,i)=>{
         if(library[e] == undefined) library[e] = {}
         if(library[e][words[i+1]] == undefined) library[e][words[i+1]] = 0
         library[e][words[i+1]] ++
-        // console.log('working..')
-    console.log(library)
     })
 }
 
 const storeTwo = function(){
-    document.getElementById('user-input').value = 'loading...'
     const words = getWords(source)
-    console.log(words)
     words.forEach((e,i)=>{
-        console.log(words.length)
         if(i < words.length - 3){
             const two = `${e} ${words[i+1]}`
             if(library[two] == undefined) library[two] = {}
@@ -35,60 +31,161 @@ const storeTwo = function(){
             library[two][words[i+2]] ++
         } 
     })
+    store()
+    storeThree()
     document.getElementById('user-input').value = ''
-    $('#submit')
-        .text('ready')
-        .css({'background-color':'green','color':'white','padding':'2px 4px','border':'0px'})
+    $('#submit').text('ready').css({'background-color':'green','color':'white','padding':'2px 4px','border':'0px'})
     $('#user-input').css('display','inline-block')
     $('#suggestion-container').css('display','block')
-    console.log(library)
 }
 
-async function load(){
-    // $('#loading').text('loading')
-    return 1
+const storeThree = function(){
+    const words = getWords(source)
+    words.forEach((e,i)=>{
+        if(i<words.length-3){
+            if(threeLibrary[e] == undefined) threeLibrary[e] = {}
+            if(threeLibrary[e][words[i+2]] == undefined) threeLibrary[e][words[i+2]] = 0
+            threeLibrary[e][words[i+2]] ++
+        }
+    })
+    console.log(threeLibrary)
 }
 
+ async function suggestThree(){
+    const words = getWords(userInput)
+    const choices = []
+    if(words.length < 2){return}
+    const word = words[words.length - 2]
+    if(library[word] !== undefined){
+        //console.log('three running suggest')
+        const suggestions = library[word]
+        for(let w in suggestions){
+            if(suggestions[w] > 4){
+                for(let i = 0; i<suggestions[w];i++){
+                    choices.push(w)
+                }
+            }
+        }
+    }
+    //console.log(choices, 'three choices')
+    return choices
+}
 
+$('#submit').click(function(){storeTwo()})
 
-$('#submit').click(function(){load().then(storeTwo())})
-
-const suggest = e => {
-    console.log(e)
-    if(e.keyCode !== 32){return}
+async function suggest(e) {
+    // if(e.keyCode !== 32){return}
+    const choices = []
     const words = getWords(userInput),
         word = words[words.length - 1]
     suggestions = library[word]
     if(suggestions == undefined){ return }
     const r = [0,'']
     for(let w in suggestions){
+        for(let i = 0; i<suggestions[w]; i++){
+            choices.push(w)
+        }
         if(suggestions[w] > r[0]){ r[0] = suggestions[w]; r[1] = w} 
     }
-    document.getElementById('user-input').value += r[1].length ? ` ${r[1]}` : '' 
-    // return r[1]
+    // document.getElementById('user-input').value += r[1].length ? ` ${r[1]}` : '' 
+    return choices
+}
+
+
+async function sentenceAnalysis(choices){
+    const url = 'https://language.googleapis.com/v1beta2/documents:analyzeSyntax '
+    // analyze content of sentence
+
+    // get sentence
+
+    const sentences = getWords(userInput,false).join(' ').split('.')
+    const sentence = sentences[sentences.length - 1]
+    const newSentence = sentence.length ? false : true
+
+    const req = {
+        // document: {object('this is a sample sentence')},
+
+    }
+
+    $.post(url,{})
+
+    console.log(sentence, sentences)
+
+        // determine if begining of sentence
+
+    // send sentence to API
+
+    // tree for sentence structure
+
+    // determine next word type(s) based on sentence
+
+    // analyze choices to find best fit
+    // if there are multiple acceptable options choose most heavily weighted
+
+    // return next word and period if is end of sentence 
 }
 
 const suggestTwo = e => {
+
     $('#submit').text('submit').css({'background-color':'white','color':'black','border':'1px solid black'})
-    console.log(e)
+    console.log(e.keyCode, e.keyCode == 32)
     // if(e.keyCode !== 39){ return }
-    // if(e.keyCode !== 39 || e.keyCode !== 32){ return }
+    if(e.keyCode !== 39 && e.keyCode !== 32){ return }
+    let choices = []
     const words = getWords(userInput)
-    if(words.length <= 1){ return }
+    // if(words.length <= 1){ return }
     const two = `${words[words.length - 2]} ${words[words.length - 1]}`
-    if(library[two] == undefined){ return }
-    const r = [0,'']
+    if(library[words[words.length - 1]] == undefined){ return }
     const suggestions = library[two]
     for(let word in suggestions){
-        if(suggestions[word] > r[0]){ r[0] = suggestions[word]; r[1] = word}
+        for(let i = 0; i<suggestions[word]; i++){
+            choices.push(word, word)
+        }
     }
-    $('#suggestion').text(r[1])
-    console.log($('#suggestion'))
-    if(e.keyCode !== 39){return}
-    document.getElementById('user-input').value += ` ${r[1]} `
-    $('#suggestion').text('')
-    suggestTwo()
-    console.log(r[1])
+    // $('#suggestion').text(r[1])
+    // console.log($('#suggestion'))
+    // if(e.keyCode !== 39){return}
+
+
+    const key = e.keyCode
+    suggest().then(d => {
+        // console.log(choices)
+        // console.log(d)
+        // choices = choices.concat(d)
+        suggestThree().then(t => {
+            // console.log(t)
+            //console.log(choices)
+            const nchoices = choices.concat(d) // .concat(d)
+
+            //
+
+            // sentenceAnalysis()
+
+            // 
+            // console.log(nchoices, 'nchoices')
+            const clist = {}
+            nchoices.forEach(e=>{
+                clist[e] == undefined ?
+                    clist[e] = 0 :
+                    clist[e] ++
+            })
+            const choice = nchoices[Math.floor(Math.random() * nchoices.length)]
+            // console.log(choice)
+            $('#suggestion').text(choice)
+            if(key !== 39){ return }
+            document.getElementById('user-input').value += ` ${choice} `
+            $('#suggestion').text('')
+            suggestTwo()
+        })
+    })
+    // console.log(one)
+    // console.log(choices)
+    // choices.concat(one)
+    // console.log(choices)
+    // document.getElementById('user-input').value += ` ${r[1]} `
+    // $('#suggestion').text('')
+    // suggestTwo()
+    // console.log(r[1])
 }
 
 $('#user-input').keyup(e=>suggestTwo(e))
